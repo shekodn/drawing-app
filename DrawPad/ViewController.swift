8c2cd2c74588c8b7fde48b501671f7b3665a962e
shekodn <sdiazn19@gmail.com>
2017-04-21 15:30:57 -0500

    adds project

---
+++ DrawPad/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  DrawPad
+//
+//  Created by Jean-Pierre Distler on 13.11.14.
+//  Copyright (c) 2014 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    //lastPoint stores the last drawn point on the canvas. This is used when a continuous brush stroke is being drawn on the canvas.
+    var lastPoint: CGPoint!
+
+
+    //red, green, and blue store the current RGB values of the selected color.
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    
+    //brushWidth and opacity store the brush stroke width and opacity.
+    var brushWidth: CGFloat = 5.0
+    var opacity: CGFloat = 1.0
+    
+    //swiped identifies if the brush stroke is continuous.
+    var swiped = false
+    
+    
+    
+    //This builds up an array of RGB values, where each array element is a tuple of three
+    // CGFloats. The colors here match the order of the colors in the interface as well as each 
+    //button’s tag.
+
+    let colors: [(CGFloat, CGFloat, CGFloat)] = [
+        (0, 0, 0),
+        (105.0 / 255.0, 105.0 / 255.0, 105.0 / 255.0),
+        (1.0, 0, 0),
+        (0, 0, 1.0),
+        (51.0 / 255.0, 204.0 / 255.0, 1.0),
+        (102.0 / 255.0, 204.0 / 255.0, 0),
+        (102.0 / 255.0, 1.0, 0),
+        (160.0 / 255.0, 82.0 / 255.0, 45.0 / 255.0),
+        (1.0, 102.0 / 255.0, 0),
+        (1.0, 1.0, 0),
+        (1.0, 1.0, 1.0),
+        ]
+
+
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+
+    
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        /*
+         * 1
+         * The first method is responsible for drawing a line between two points. Here you want to draw
+         * into tempImageView, so you need to set up a drawing context with the image currently in the
+         * tempImageView (which should be empty the first time).
+         */
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        // 2
+        /*
+         * Next, you get the current touch point and then draw a line with CGContextAddLineToPoint from
+         * lastPoint to currentPoint. You might think that this approach will produce a series of
+         * straight lines and the result will look like a set of jagged lines. This will produce 
+         * straight lines, but the touch events fire so quickly that the lines are short enough and the
+         * result will look like a nice smooth curve.
+         */
+        context?.move(to: CGPoint(x: fromPoint.x , y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x , y: toPoint.y))
+        
+        
+        //3 Here are all the drawing parameters for brush size and opacity and brush stroke color.
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(brushWidth)
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: 1.0).cgColor)
+        context?.setBlendMode(CGBlendMode.normal)
+        
+        //4 This is where the magic happens, and where you actually draw the path!
+        context?.strokePath()
+        
+        //5 Next, you need to wrap up the drawing context to render the new line into the temporary image view.
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+
+
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //7 In touchesMoved, you set swiped to true so you can keep track of whether there is a current swipe in progress. Since this is touchesMoved, the answer is yes, there is a swipe in progress! You then call the helper method you just wrote to draw the line.
+
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            //Finally, you update the lastPoint so the next touch event will continue where you just left off.
+            lastPoint = currentPoint
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+        
+    }
+
+    
+
+
+    // MARK: - Actions
+    
+
+    @IBAction func reset(_ sender: AnyObject) {
+    
+        self.tempImageView.image = nil
+    }
+
+    @IBAction func share(_ sender: AnyObject) {
+    
+    }
+      
+    @IBAction func pencilPressed(_ sender: AnyObject) {
+        
+    
+        var index = sender.tag ?? 0
+        
+        if index < 0 || index >= colors.count {
+            index = 0
+        }
+        
+        // 2
+        (red, green, blue) = colors[index]
+        
+        // 3
+        if index == colors.count - 1 {
+            opacity = 1.0
+        }
+
+    
+    }
+}
+
